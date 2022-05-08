de66a622ba521238c594361e2ddf9e54b15d577a
Muhaimin Rezki <mrmarsaoli@gmail.com>
2022-05-08 16:10:35 +0900

    Add build file

---
+++ app/src/main/java/app/vercel/pwa_demo_neon/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package app.vercel.pwa_demo_neon.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+

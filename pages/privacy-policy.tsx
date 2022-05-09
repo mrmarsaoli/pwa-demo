b475bc6f746ba226465d1d33946f113714558f63
Muhaimin Rezki <mrmarsaoli@gmail.com>
2022-05-09 11:16:40 +0900

    new build

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,285 @@
+import { NextPage } from "next";
+
+const PrivacyPolicy: NextPage = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <h2 className="text-4xl font-bold">PRIVACY NOTICE</h2>
+
+      <span className="font-bold text-xs text-gray-700">
+        Last updated May 08, 2022
+      </span>
+
+      <div className="text-base text-gray-800">
+        <p>
+          This privacy notice for Mrmarsaoli (&quote; Company ,&quote;
+          &quote;we,&quote; &quote;us,&quote; or &quote;our&quote; ), describes
+          how and why we might collect, store, use, and/or share (
+          &quote;process&quote;) your information when you use our services
+          (&quote;Services&quote;), such as when you:
+        </p>
+        <ul>
+          <li>
+            Download and use our mobile application (PWA Demo) , or any other
+            application of ours that links to this privacy notice
+          </li>
+          <li>
+            Engage with us in other related ways, including any sales,
+            marketing, or events
+          </li>
+        </ul>
+        <p>
+          Questions or concerns? Reading this privacy notice will help you
+          understand your privacy rights and choices. If you do not agree with
+          our policies and practices, please do not use our Services. If you
+          still have any questions or concerns, please contact us at
+          mrmarsaoli@gmail.com.
+        </p>
+        <h3 className="text-2xl font-bold">SUMMARY OF KEY POINTS</h3>
+        <p>
+          This summary provides key points from our privacy notice, but you can
+          find out more details about any of these topics by clicking the link
+          following each key point or by using our table of contents below to
+          find the section you are looking for. You can also click here to go
+          directly to our table of contents.
+        </p>
+        <p>
+          What personal information do we process? When you visit, use, or
+          navigate our Services, we may process personal information depending
+          on how you interact with Mrmarsaoli and the Services, the choices you
+          make, and the products and features you use. Click here to learn more.
+        </p>
+        <p>
+          Do we process any sensitive personal information? We do not process
+          sensitive personal information.
+        </p>
+        <p>
+          Do you receive any information from third parties? We do not receive
+          any information from third parties.
+        </p>
+        <p>
+          How do you process my information? We process your information to
+          provide, improve, and administer our Services, communicate with you,
+          for security and fraud prevention, and to comply with law. We may also
+          process your information for other purposes with your consent. We
+          process your information only when we have a valid legal reason to do
+          so. Click here to learn more.
+        </p>
+        <p>
+          In what situations and with which parties do we share personal
+          information? We may share information in specific situations and with
+          specific third parties. Click here to learn more.
+        </p>
+        <p>
+          How do we keep your information safe? We have organizational and
+          technical processes and procedures in place to protect your personal
+          information. However, no electronic transmission over the internet or
+          information storage technology can be guaranteed to be 100% secure, so
+          we cannot promise or guarantee that hackers, cybercriminals, or other
+          unauthorized third parties will not be able to defeat our security and
+          improperly collect, access, steal, or modify your information. Click
+          here to learn more.
+        </p>
+        <p>
+          What are your rights? Depending on where you are located
+          geographically, the applicable privacy law may mean you have certain
+          rights regarding your personal information. Click here to learn more.
+        </p>
+        <p>
+          How do I exercise my rights? The easiest way to exercise your rights
+          is by filling out our data subject request form available here , or by
+          contacting us. We will consider and act upon any request in accordance
+          with applicable data protection laws.
+        </p>
+        <p>
+          Want to learn more about what Mrmarsaoli does with any information we
+          collect? Click here to review the notice in full.
+        </p>
+        <h3 className="text-2xl font-bold">TABLE OF CONTENTS</h3>
+        <ol>
+          <li>
+            <a href="#">WHAT INFORMATION DO WE COLLECT?</a>
+          </li>
+          <li>
+            <a href="#">HOW DO WE PROCESS YOUR INFORMATION?</a>
+          </li>
+          <li>
+            <a href="#">
+              WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+            </a>
+          </li>
+          <li>
+            <a href="#">HOW LONG DO WE KEEP YOUR INFORMATION?</a>
+          </li>
+          <li>
+            <a href="#">HOW DO WE KEEP YOUR INFORMATION SAFE?</a>
+          </li>
+          <li>
+            <a href="#">DO WE COLLECT INFORMATION FROM MINORS?</a>
+          </li>
+          <li>
+            <a href="#">WHAT ARE YOUR PRIVACY RIGHTS?</a>
+          </li>
+          <li>
+            <a href="#">CONTROLS FOR DO-NOT-TRACK FEATURES</a>
+          </li>
+          <li>
+            <a href="#">
+              DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
+            </a>
+          </li>
+          <li>
+            <a href="#">DO WE MAKE UPDATES TO THIS NOTICE?</a>
+          </li>
+          <li>
+            <a href="#">HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</a>
+          </li>
+          <li>
+            <a href="#">
+              HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM
+              YOU?
+            </a>
+          </li>
+        </ol>
+        1. WHAT INFORMATION DO WE COLLECT? Personal information you disclose to
+        us In Short: We collect personal information that you provide to us. We
+        collect personal information that you voluntarily provide to us when you
+        express an interest in obtaining information about us or our products
+        and Services, when you participate in activities on the Services, or
+        otherwise when you contact us. Sensitive Information. We do not process
+        sensitive information. Application Data. If you use our application(s),
+        we also may collect the following information if you choose to provide
+        us with access or permission: Push Notifications. We may request to send
+        you push notifications regarding your account or certain features of the
+        application(s). If you wish to opt out from receiving these types of
+        communications, you may turn them off in your device&apos;s settings.
+        This information is primarily needed to maintain the security and
+        operation of our application(s), for troubleshooting, and for our
+        internal analytics and reporting purposes. All personal information that
+        you provide to us must be true, complete, and accurate, and you must
+        notify us of any changes to such personal information. 2. HOW DO WE
+        PROCESS YOUR INFORMATION? In Short: We process your information to
+        provide, improve, and administer our Services, communicate with you, for
+        security and fraud prevention, and to comply with law. We may also
+        process your information for other purposes with your consent. We
+        process your personal information for a variety of reasons, depending on
+        how you interact with our Services, including: 3. WHEN AND WITH WHOM DO
+        WE SHARE YOUR PERSONAL INFORMATION? In Short: We may share information
+        in specific situations described in this section and/or with the
+        following third parties. We may need to share your personal information
+        in the following situations: Business Transfers. We may share or
+        transfer your information in connection with, or during negotiations of,
+        any merger, sale of company assets, financing, or acquisition of all or
+        a portion of our business to another company. 4. HOW LONG DO WE KEEP
+        YOUR INFORMATION? In Short: We keep your information for as long as
+        necessary to fulfill the purposes outlined in this privacy notice unless
+        otherwise required by law. We will only keep your personal information
+        for as long as it is necessary for the purposes set out in this privacy
+        notice, unless a longer retention period is required or permitted by law
+        (such as tax, accounting, or other legal requirements). No purpose in
+        this notice will require us keeping your personal information for longer
+        than __________ . When we have no ongoing legitimate business need to
+        process your personal information, we will either delete or anonymize
+        such information, or, if this is not possible (for example, because your
+        personal information has been stored in backup archives), then we will
+        securely store your personal information and isolate it from any further
+        processing until deletion is possible. 5. HOW DO WE KEEP YOUR
+        INFORMATION SAFE? In Short: We aim to protect your personal information
+        through a system of organizational and technical security measures. We
+        have implemented appropriate and reasonable technical and organizational
+        security measures designed to protect the security of any personal
+        information we process. However, despite our safeguards and efforts to
+        secure your information, no electronic transmission over the Internet or
+        information storage technology can be guaranteed to be 100% secure, so
+        we cannot promise or guarantee that hackers, cybercriminals, or other
+        unauthorized third parties will not be able to defeat our security and
+        improperly collect, access, steal, or modify your information. Although
+        we will do our best to protect your personal information, transmission
+        of personal information to and from our Services is at your own risk.
+        You should only access the Services within a secure environment. 6. DO
+        WE COLLECT INFORMATION FROM MINORS? In Short: We do not knowingly
+        collect data from or market to children under 18 years of age. We do not
+        knowingly solicit data from or market to children under 18 years of age.
+        By using the Services, you represent that you are at least 18 or that
+        you are the parent or guardian of such a minor and consent to such minor
+        dependent’s use of the Services. If we learn that personal information
+        from users less than 18 years of age has been collected, we will
+        deactivate the account and take reasonable measures to promptly delete
+        such data from our records. If you become aware of any data we may have
+        collected from children under age 18, please contact us at
+        mrmarsaoli@gmail.com . 7. WHAT ARE YOUR PRIVACY RIGHTS? In Short: You
+        may review, change, or terminate your account at any time. If you are
+        located in the EEA or UK and you believe we are unlawfully processing
+        your personal information, you also have the right to complain to your
+        local data protection supervisory authority. You can find their contact
+        details here:
+        https://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm.
+        If you are located in Switzerland, the contact details for the data
+        protection authorities are available here:
+        https://www.edoeb.admin.ch/edoeb/en/home.html. Withdrawing your consent:
+        If we are relying on your consent to process your personal information,
+        which may be express and/or implied consent depending on the applicable
+        law, you have the right to withdraw your consent at any time. You can
+        withdraw your consent at any time by contacting us by using the contact
+        details provided in the section &quote; HOW CAN YOU CONTACT US ABOUT
+        THIS NOTICE? &quote; below. However, please note that this will not
+        affect the lawfulness of the processing before its withdrawal, nor when
+        applicable law allows, will it affect the processing of your personal
+        information conducted in reliance on lawful processing grounds other
+        than consent. If you have questions or comments about your privacy
+        rights, you may email us at mrmarsaoli@gmail.com. 8. CONTROLS FOR
+        DO-NOT-TRACK FEATURES Most web browsers and some mobile operating
+        systems and mobile applications include a Do-Not-Track (
+        &quote;DNT&quote;) feature or setting you can activate to signal your
+        privacy preference not to have data about your online browsing
+        activities monitored and collected. At this stage no uniform technology
+        standard for recognizing and implementing DNT signals has been finalized
+        . As such, we do not currently respond to DNT browser signals or any
+        other mechanism that automatically communicates your choice not to be
+        tracked online. If a standard for online tracking is adopted that we
+        must follow in the future, we will inform you about that practice in a
+        revised version of this privacy notice. 9. DO CALIFORNIA RESIDENTS HAVE
+        SPECIFIC PRIVACY RIGHTS? In Short: Yes, if you are a resident of
+        California, you are granted specific rights regarding access to your
+        personal information. California Civil Code Section 1798.83, also known
+        as the &quote;Shine The Light&quote; law, permits our users who are
+        California residents to request and obtain from us, once a year and free
+        of charge, information about categories of personal information (if any)
+        we disclosed to third parties for direct marketing purposes and the
+        names and addresses of all third parties with which we shared personal
+        information in the immediately preceding calendar year. If you are a
+        California resident and would like to make such a request, please submit
+        your request in writing to us using the contact information provided
+        below. If you are under 18 years of age, reside in California, and have
+        a registered account with Services, you have the right to request
+        removal of unwanted data that you publicly post on the Services. To
+        request removal of such data, please contact us using the contact
+        information provided below and include the email address associated with
+        your account and a statement that you reside in California. We will make
+        sure the data is not publicly displayed on the Services, but please be
+        aware that the data may not be completely or comprehensively removed
+        from all our systems (e.g., backups, etc.). 10. DO WE MAKE UPDATES TO
+        THIS NOTICE? In Short: Yes, we will update this notice as necessary to
+        stay compliant with relevant laws. We may update this privacy notice
+        from time to time. The updated version will be indicated by an updated
+        &quote;Revised&quote; date and the updated version will be effective as
+        soon as it is accessible. If we make material changes to this privacy
+        notice, we may notify you either by prominently posting a notice of such
+        changes or by directly sending you a notification. We encourage you to
+        review this privacy notice frequently to be informed of how we are
+        protecting your information. 11. HOW CAN YOU CONTACT US ABOUT THIS
+        NOTICE? If you have questions or comments about this notice, you may
+        email us at mrmarsaoli@gmail.com or by post to: Mrmarsaoli Jln. Durian 1
+        Jati Perumnas Ternate , Maluku Utara 97713 Indonesia 12. HOW CAN YOU
+        REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU? Based on the
+        applicable laws of your country, you may have the right to request
+        access to the personal information we collect from you, change that
+        information, or delete it in some circumstances. To request to review,
+        update, or delete your personal information, please submit a request
+        form by clicking here . This privacy policy was created using
+        Termly&apos;s Privacy Policy Generator.
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
